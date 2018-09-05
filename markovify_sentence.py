9a0723128149ae84938c8f0535b15f25376ff360
0-jam <0jamaozm1137@gmail.com>
2018-09-05 16:49:49 +0900

    文章生成後にカラの要素を削除するようにした

--- markovify_sentence.py
+++ markovify_sentence.py
@@ -1,23 +1,22 @@
-### マルコフ連鎖で文章つくって表示
-# usage: $ python marcovify_tweet.py <FILENAME>
-
 import sys
 import markovify
 
-## ファイル読む
+## ファイル読んでマルコフ連鎖モデル作成
 def marcovify_text(file):
     with open(file) as str:
         text = str.read()
 
-    # 読んだテキストを表示
-    # print(text)
-
-    # 読んだテキストからマルコフ連鎖でモデル？生成
     return markovify.Text(text)
 
 model = marcovify_text(sys.argv[1])
 
 with open('out.txt', 'w') as out:
+    arr = []
     for i in range(10):
-        # 作ったモデルから適当に文章つくってファイルに書き込む
-        out.write("".join(model.make_sentence()))
+        # 作ったモデルから文章生成
+        arr.append(model.make_sentence())
+
+    # カラ(NoneType)の要素を削除
+    arr = list(filter(None, arr))
+    # ファイルに書き込む
+    out.write("\n".join(arr))
--- wakachi.py
+++ wakachi.py
@@ -1,6 +1,3 @@
-### 前処理用スクリプト
-# Janomeを使って読み込んだ文章を分かち書き
-
 import sys
 from janome.tokenizer import Tokenizer
 
@@ -9,9 +6,11 @@ t = Tokenizer(wakati=True)
 
 arr = []
 
+# 読み込んだ文章を分かち書き
 with open(sys.argv[1]) as str:
     arr.append(t.tokenize(str.read()))
 
+# 半角スペース区切りでファイルに書き込む
 with open(sys.argv[2], 'w') as out:
     for str in arr:
         out.write(" ".join(str))
