6b500d9d3e9f4cf6179d2cd49f5a08bd207ce8f2
0-jam <0jamaozm1137@gmail.com>
2018-12-19 15:01:44 +0900

    Changed divide_word() to return list of words

--- modules/wakachi/janome.py
+++ modules/wakachi/janome.py
@@ -2,10 +2,5 @@ from janome.tokenizer import Tokenizer
 t = Tokenizer(wakati=True)
 
 ## 文sentenceを分かち書き
-# delimiter（デフォルトは半角スペース）で単語どうしをつなぐ
-def divide_word(sentence, delimiter=" "):
-    return delimiter.join(
-        list(filter(
-            lambda line: line != " ", t.tokenize(sentence.strip())
-        ))
-    )
+def divide_word(sentence):
+    return list(filter(lambda line: line != " ", t.tokenize(sentence.strip())))
--- modules/wakachi/juman.py
+++ modules/wakachi/juman.py
@@ -3,7 +3,7 @@ juman = Juman()
 
 ## 文sentenceを分かち書き
 # BUG: "ダブルクオーテーション"を含む文字列（'".'など）を投げるとその部分が二重になって返ってくる
-def divide_word(sentence, delimiter=" "):
+def divide_word(sentence):
     sentence = sentence.strip()
     # Juman++に空文字列を投げると固まってしまう
     if not sentence:
@@ -15,4 +15,4 @@ def divide_word(sentence, delimiter=" "):
     divided_sentence = [word.midasi for word in words]
     divided_sentence = list(filter(lambda line: line != "\u3000", divided_sentence))
 
-    return delimiter.join(divided_sentence)
+    return divided_sentence
